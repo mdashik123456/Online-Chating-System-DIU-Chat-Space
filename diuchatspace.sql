22a2b96d8d57437662b05afab1c651b31adcb40d
mdashik123456 <mdashik1404@gmail.com>
2023-09-25 20:17:38 +0600

    database

--- diuchatspace.sql
+++ diuchatspace.sql
@@ -49,17 +49,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `gender` varchar(7) NOT NULL,
-  `isLoggedIn` varchar(5) NOT NULL DEFAULT 'no',
-  `profile_pic` varchar(255) NOT NULL
+  `isLoggedIn` varchar(15) NOT NULL DEFAULT 'Not Active',
+  `profile_pic` varchar(255) NOT NULL,
+  `bio` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `name`, `email`, `password`, `gender`, `isLoggedIn`, `profile_pic`) VALUES
-(1, 'mac', 'mac ashik', 'mac@mac.com', '$2y$10$JN4C7sFquKR/kGN9EkCIv.FRFLMhfDBoryeN27JvKzOGTa4OOVtwS', 'Male', 'yes', './images/mac.jpeg'),
-(2, 'pc', 'pc ashik', 'pc@pc.com', '$2y$10$akHm/HsOn7xWkjuXpn11jeMwG3TGiszWaSqKXRaOWSY8RFSaII6Me', 'Male', 'yes', './images/pc.jpeg');
 
 --
 -- Indexes for dumped tables
