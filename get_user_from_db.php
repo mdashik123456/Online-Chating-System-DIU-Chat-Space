e8db682b6aacf7f27879729a7397cd3e201268aa
mdashik123456 <mdashik1404@gmail.com>
2023-09-26 22:05:03 +0600

    update

--- get_user_from_db.php
+++ get_user_from_db.php
@@ -12,8 +12,8 @@ while ($row = mysqli_fetch_assoc($result)) {
     if ($_SESSION["username"] !== $row["username"]) {
         $output .= "<tbody>
                 <tr>
-                    <td><img src='".$row['profile_pic']."' class='img-thumbnail' style='height: 70px; width:70px;'></td>
-                    <td><p>" . $row["name"]. "<br>".$row["bio"]."</p></td>
+                    <td><a href='?incoming_user=".$row['username']."'><img src='".$row['profile_pic']."' class='img-thumbnail' style='height: 70px; width:70px;'></a></td>
+                    <td><a class='user_link' href='?incoming_user=".$row['username']."'><p>" . $row["name"]. "<br>".$row["bio"]."</p></a></td>
                     <td><i id='active_status' class='fa-solid fa-circle fa-2xs'></i></td>
                 </tr>
             </tbody>";
