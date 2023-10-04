419437085fcf62eda205447b9e517cb4f0723a5c
mdashik123456 <mdashiik1404@gmail.com>
2023-10-04 13:10:50 +0600

    update

--- connection.php
+++ connection.php
@@ -1,5 +1,4 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "root", "diuchatspace") or die("Connection was not established");
-// $conn = mysqli_connect("localhost", "root", "", "diuchatspace") or die("Connection was not established");
+$conn = mysqli_connect("localhost", "root", "", "diuchatspace") or die("Connection was not established");
 ?>
